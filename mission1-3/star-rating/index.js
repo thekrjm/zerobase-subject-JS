a60c72e9b6acd2fb18d0f0147f4934f2bb91f084
Jeman Ryu <thekrjm@naver.com>
2022-11-24 19:23:59 +0900

    1차 코딩

--- mission1-3/star-rating/index.js
+++ mission1-3/star-rating/index.js
@@ -1,2 +1,18 @@
 // do something!
 
+const star = get('.star-rating')
+
+export const StarRating=($container)=>{
+    const starRatingContainer = document.createElement('div',{
+        className:'star-rating-container'
+    })
+
+    star.classList.add('star-rating-container');
+
+    const createStar= document.createElement('i',{
+        className:'bx bxs-star hovered',
+    });
+
+    starRatingContainer.appendChild(createStar);
+    document.body.appendChild(starRatingContainer);
+}
